91016bb3efc1b93c17598fa10e4f7452a66ef3c7
Benjamin Farthing <benjamin.farthing@gmail.com>
2025-09-09 13:27:43 -0400

    chore: GH Pages deploy workflow + Vite base path for project pages

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,8 +1,9 @@
+/* eslint-env node */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/proj-green-careers-map/", // GitHub Pages project path
   server: { port: 4000 },
 });
